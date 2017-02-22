a412cca9a4dbf7cbe23b14b418a2d8d29d16016d
Nick <nick@spf-hup.0735.wlan.wireless-pennnet.upenn.edu>
2017-02-22 12:35:25 -0500

    working solution for max sale problem, including negative profits

--- MaxDiff.py
+++ MaxDiff.py
@@ -25,8 +25,18 @@ assert(max_difference([0, 2, 3, 4, 5, 6, 7, 800, 9, 10]) == 800)
 def max_sale(ls):
     """
     Return the indices that provide the greatest sale price.
-
-    For index i, return the max of index (i - j), where j is an index
-    of a number that came before i.
     """
-    pass
+    min_price = ls[0]
+    max_sale = ls[1] - ls[0]
+
+    for i in range(1, len(ls)):
+        min_price = min(min_price, ls[i - 1])
+        current_sale = ls[i] - min_price
+        max_sale = max(current_sale, max_sale)
+    return max_sale
+
+
+assert(max_sale([5, 2, 5, 3, 4, 5, 6, 7, 7, 8, 9]) == 7)
+assert(max_sale([5, 2, 5, 3, 4, 5, 60, 7, 7, 8, 9]) == 58)
+assert(max_sale([9, 7, 4, 3, 1]) == -1)
+assert(max_sale([9, 7, 4, 2, 1]) == -1)
